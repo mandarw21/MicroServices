1d37112d1d8a6d398bbf788c9fc88e906a7bc261
Mandar Wadnerkar <34623812+mandarw21@users.noreply.github.com>
2019-08-13 07:18:34 -0700

    Add files via upload

---
+++ Microservices/Currency-Conversion/src/main/java/com/zensar/currency/conversionr/CurrencyConversionController.java
@@ -0,0 +1,35 @@
+package com.zensar.currency.conversionr;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurency(@PathVariable String from,
+			@PathVariable String to,@PathVariable BigDecimal quantity) {
+		
+		Map<String, String> uriVariablesMap = new HashMap<String, String>();
+		uriVariablesMap.put("from", from);
+		uriVariablesMap.put("to", to);
+		
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}/",CurrencyConversionBean.class,uriVariablesMap);
+		
+		CurrencyConversionBean response = responseEntity.getBody();
+		
+		
+		return new CurrencyConversionBean(response.getId(),from,to,
+				response.getConversionMultiple(),quantity,
+				quantity.multiply(response.getConversionMultiple()),
+				response.getPort());
+	}
+}
---
+++ Microservices/ForEx-Service/src/main/java/com/zensar/forex/ForExServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForExServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForExServiceApplication.class, args);
+	}
+
+}
